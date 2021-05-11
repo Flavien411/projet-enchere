7b8a642ecdcef78dfcf7c29632094d4a3756fd53
Flav <flaviencoulmeau@gmail.com>
2021-05-11 10:40:55 +0200

    Import debut Navbar

---
+++ src/components/navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: 'Enchères',
+        url:'#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Vendre un bien',
+        url:'#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Mon Profil',
+        url:'#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Connexion',
+        url:'#',
+        cName: 'nav-links-mobile'
+    },
+]
